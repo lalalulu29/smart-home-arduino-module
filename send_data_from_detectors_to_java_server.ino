ab3db14138edcb0d6dccb9093b8a5c3df9fbd909
Kirill Lyubarskikh <kelubarskih@fintechiq.ru>
2024-09-23 22:13:22 +0200

    add initial setting view

--- send_data_from_detectors_to_java_server.ino
+++ send_data_from_detectors_to_java_server.ino
@@ -4,6 +4,31 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Настройки модуля</title>
+</head>
+<body>
+    <h3>Настройки модуля</h3>
+    
+    <form action="/updateSettings" method="POST">
+        <label for="wifiName">Наименование Wi-Fi:</label><br>
+        <input type="text" id="wifiName" name="wifiName" required><br><br>
+        
+        <label for="wifiPassword">Пароль от Wi-Fi:</label><br>
+        <input type="password" id="wifiPassword" name="wifiPassword" required><br><br>
+        
+        <button type="submit">Обновить настройки</button>
+    </form>
+</body>
+</html>
+)rawliteral";
+
+
 bool isInitialMode = false;
 
 AsyncWebServer server(8080);
@@ -41,9 +66,20 @@ void initialWifiMode() {
 
 void initialWebServerMode() {
   
-
   server.on("/", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "OK");
+    request->send_P(200, "text/html", index_html);
+  });
+
+  server.on("/updateSettings", HTTP_POST, [] (AsyncWebServerRequest *request) {
+    AsyncWebParameter * j = request->getParam(0);
+    Serial.print("login: ");
+    Serial.println(j->value()); 
+
+    AsyncWebParameter * l = request->getParam(1);
+    Serial.print("Password: ");
+    Serial.println(l->value()); 
+
+    request->send_P(200, "text/plain", "OK");
   });
 
   server.begin();
