5a8045741b8f902287dc94f6f29ab47062edf815
Kirill Lyubarskikh <vil.rik@mail.ru>
2025-10-23 20:42:54 +0200

    Update module to esp32 and add support MHZ 19

--- send_data_from_detectors_to_java_server.ino
+++ send_data_from_detectors_to_java_server.ino
@@ -1,9 +1,10 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <Hash.h>
-#include <ESPAsyncTCP.h>
+#include <WiFi.h>
+
+
+#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <ESP8266HTTPClient.h>
+#include <HTTPClient.h>
 #include <EEPROM.h>
 
 #include <Wire.h>
@@ -11,12 +12,17 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+#include <Mhz19.h>
+#include <HardwareSerial.h>
+
+// #define BME_SCK 26
+// #define BME_MISO 12
+// #define BME_MOSI 11
+// #define BME_CS 10
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define RXD2 16
+#define TXD2 17
 
 
 String publicWifiLogin = "";
@@ -26,6 +32,9 @@ String publicServerKey = "";
 String publicUpdateTimer = "";
 
 Adafruit_BME280 bme;
+
+HardwareSerial hardwareSerial(2);
+Mhz19 sensor;
 
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
@@ -52,16 +61,19 @@ AsyncWebServer server(8080);
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  hardwareSerial.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Wire.begin(21, 22);
   EEPROM.begin(512);
   delay(100);
   initializePublicVariable();
 
-  pinMode(D0, INPUT);
+  pinMode(13, INPUT);
 
-  isInitialMode = digitalRead(D0);
+  isInitialMode = digitalRead(13);
 
   if (isInitialMode) {
+    Serial.println("Started in initial mode");
     initialWifiMode();
     initialWebServerMode();
   } else {
@@ -80,13 +92,35 @@ void loop() {
       HTTPClient http;
 
       http.begin(client, "http://" + publicUrlServer + "/sendData");
-
       http.addHeader("Content-Type", "application/json");
+
+      delay(500);
+      int carbonDioxide = sensor.getCarbonDioxide();
+      if (carbonDioxide >= 0) {
+        delay(100);
+        int firstDataFromCarbonDioxideCensor = sensor.getCarbonDioxide();
+        delay(100);
+        int secondDataFromCarbonDioxideCensor = sensor.getCarbonDioxide();
+        delay(100);
+        int thirdDataFromCarbonDioxideCensor = sensor.getCarbonDioxide();
+
+        carbonDioxide = (firstDataFromCarbonDioxideCensor + secondDataFromCarbonDioxideCensor + thirdDataFromCarbonDioxideCensor) / 3;
+        Serial.print("Got data from carbon dioxide sencer: ");
+        Serial.println(String(carbonDioxide) + "ppm");
+      } else {
+        Serial.println("Couldnt get data from carbon dioxide sencer");
+      }
+
       String request = "{ \"temperature\": " + String(bme.readTemperature()) +
                        ", \"pressure\": " + String((bme.readPressure() / 100.0F)) +
                        ", \"altitude\": " + String(bme.readAltitude(SEALEVELPRESSURE_HPA)) + 
                        ", \"humidity\": " + String(bme.readHumidity()) + 
+                       ", \"carbonDioxide\": " + String(carbonDioxide) +
                        "}";
+
+      
+      
+
       int httpResponseCode = http.POST(request);
 
 
@@ -97,22 +131,42 @@ void loop() {
   }
 }
 
+unsigned long waitToWiFi = 0;
+
 void initialMainMode() {
   if (publicWifiLogin != "" && publicWifiPassword != "") {
-    WiFi.begin(publicWifiLogin, publicWifiPassword);
-    while (WiFi.status() != WL_CONNECTED) { 
-      //Тут нужна какая то логика ожидания подкюлчения к ви фи
-      delay(100);
+    while(WiFi.status() != WL_CONNECTED) {
+      Serial.println("");
+      WiFi.disconnect();
+      delay(5000);
+      WiFi.begin(publicWifiLogin, publicWifiPassword);
+      Serial.print("Try to connect to Wi-Fi.");
+
+      while ((millis() - waitToWiFi) < 60000) { 
+      }
+      waitToWiFi = millis();
     }
+    
+    Serial.println("");
+    Serial.println("Connected to Wi-Fi");
     bme.begin(0x76);
+    
+    sensor.begin(&hardwareSerial);
+    sensor.setMeasuringRange(Mhz19MeasuringRange::Ppm_5000);
+    sensor.enableAutoBaseCalibration();
+
+     while (!sensor.isReady()) {
+      delay(1000);
+      Serial.println("Senson still doesnt ready");
+    }
   }
 }
 
 
 void initialWifiMode() {
   
-  const char* ssid = "";
-  const char* password = "";
+  const char* ssid = "test_test";
+  const char* password = "12345678";
 
   IPAddress local_IP(192,168,99,100);    
   IPAddress gateway(192,168,99,10);
@@ -120,6 +174,12 @@ void initialWifiMode() {
 
   WiFi.softAPConfig(local_IP, gateway, subnet);
   WiFi.softAP(ssid, password);
+
+  Serial.println("Created WIFI point");
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  Serial.print("Password: ");
+  Serial.println(password);
 }
 
 void initialWebServerMode() {
